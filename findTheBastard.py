058db2de1159a183178b115a8a6333d7f07c8fee
Jared Gluck <jpg5733@psu.edu>
2024-01-16 22:21:42 -0800

    pls work

--- findTheBastard.py
+++ findTheBastard.py
@@ -5,6 +5,8 @@ class findTheBastard(handler.Handler):
     def can_handle(self, message):
         #print(message.channel.id, message.channel)
         if message.channel is "upper-bracket":
+            print(message.content)
+            print(message.author.name)
             return True
 
     async def handle(self, message, client, collection, cache):
