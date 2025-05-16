520fae8ec8155d9796980646afa4f76e4c58888e
TheBattleMac <110958350+TheBattleMac@users.noreply.github.com>
2025-05-17 00:05:04 +0200

    final push like to your gf

--- importantThings.py
+++ importantThings.py
@@ -11,12 +11,12 @@ class lobsters(handler.Handler):
 
     async def handle(self, message, client, collection, cache):
         roll = random.uniform(0,1)
-        if client.user.id == "995535732695379988": 
+        if client.user.id == 995535732695379988: 
             msg = "Go home Badger!"
-        elif client.user.id == "131887776646823936":
+        elif client.user.id == 131887776646823936:
             msg = "No you’re never gonna get it! -Teo"
         msg = "Shh they're sleeping"
-        if roll <= 0.1 or client.user.id == "314200471088922636":
+        if roll <= 0.1 or client.user.id == 314200471088922636:
             msg = "https://i.imgur.com/BMcur.gif"
         elif roll >= 0.9:
             msg = "lol get fuuuuuucked"
