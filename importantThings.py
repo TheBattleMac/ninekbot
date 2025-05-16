3dbc2c13a797b7c46f146f2b40e17b04d5d48510
TheBattleMac <110958350+TheBattleMac@users.noreply.github.com>
2025-05-16 23:34:52 +0200

    Very important changes to importantThings.py
    With Love from Mac

--- importantThings.py
+++ importantThings.py
@@ -11,8 +11,14 @@ class lobsters(handler.Handler):
 
     async def handle(self, message, client, collection, cache):
         roll = random.uniform(0,1)
+        if client.user.id == "995535732695379988":
+            msg = "Go home Badger!"
+        if  client.user.id == "131887776646823936":
+            msg = "lol get fuuuuuucked"
         msg = "Shh they're sleeping"
         if roll <= .01 or client.user.id == "314200471088922636":
             msg = "https://i.imgur.com/BMcur.gif"
+        elif roll <= .01:
+            msg = "lol get fuuuuuucked"
         await message.channel.send(msg)
 
