fc0033895bb050d744ae880638b379b2b1e96060
TheBattleMac <110958350+TheBattleMac@users.noreply.github.com>
2025-05-16 23:49:43 +0200

    Very important update

--- importantThings.py
+++ importantThings.py
@@ -13,8 +13,12 @@ class lobsters(handler.Handler):
         roll = random.uniform(0,1)
         if client.user.id == "995535732695379988":
             msg = "Go home Badger!"
+        elif client.user.id == "131887776646823936":
+            msg = "No you’re never gonna get it! -Teo"
         msg = "Shh they're sleeping"
-        if roll <= .1 or client.user.id == "314200471088922636":
+        if roll <= 0.1 or client.user.id == "314200471088922636":
             msg = "https://i.imgur.com/BMcur.gif"
+        elif roll <= 0.1:
+            msg = "lol get fuuuuuucked"
         await message.channel.send(msg)
 
